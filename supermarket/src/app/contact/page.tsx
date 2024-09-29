cc6ae8361dd586668aab395796eddcccf99243d6
joaobroggine <joaovitorbrogginelopes@gmail.com>
2024-09-29 17:08:54 -0300

    Finalizing the project (So Far)

---
+++ supermarket/src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function page() {
+  return (
+    <div className="flex flex-col items-center justify-center space-y-3 h-96 mb-16">
+      <h1 className="text-3xl font-bold">Contact the CEO:</h1>
+      <h2 className="text-2xl">joaovitorbrogginelopes@gmail.com</h2>
+    </div>
+  )
+}
--- supermarket/src/app/layout.tsx
+++ supermarket/src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
           <nav className="flex justify-center border p-3 m-4 border-l-transparent border-r-transparent">
         <ul className="flex flex-row space-x-9">
           <li><Link href="/" className="hover:underline">Home</Link></li>
-          <li><Link href="/pages" className="hover:underline">Pages</Link></li>
+          <li><Link href="/contact" className="hover:underline">Contact</Link></li>
           <li><Link href="/about" className="hover:underline">About us</Link></li>
           <li><Link href="/offer" className="hover:underline">Offer of the Month</Link></li>
         </ul>
